560e4996cec7545887b009bc523345e0f77238f2
Jeeses313 <jesse.vilkman@hotmail.fi>
2021-12-09 14:54:08 +0200

    Modify to send position data to server

--- Assets/Connection/ConnectionManager.cs
+++ Assets/Connection/ConnectionManager.cs
@@ -5,29 +5,50 @@ using SocketIO;
 public class ConnectionManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject go;
+    public GameObject socketIOObject;
+    public GameObject player;
+    string id;
     SocketIOComponent socket;
     void Start()
     {
-        socket = go.GetComponent<SocketIOComponent>();
+        socket = socketIOObject.GetComponent<SocketIOComponent>();
         socket.On("open", OnSocketOpen);
-        socket.On("positionsChange", OnPositionChange);
+        socket.On("positionsChange", OnPositionsChange);
     }
 
     public void OnSocketOpen(SocketIOEvent ev)
     {
         Dictionary<string, string> data = new Dictionary<string, string>();
-        data["name"] = "example";
+        data["name"] = OwnData.name;
         data["x"] = "0";
         data["y"] = "0";
         socket.Emit("initialize", new JSONObject(data));
-        Debug.Log("updated socket id " + socket.sid);
+        Debug.Log("Connected with socket id " + socket.sid);
+        id = socket.sid;
     }
 
-    public void OnPositionChange(SocketIOEvent ev)
+    public void OnPositionsChange(SocketIOEvent ev)
     {
         Debug.Log(ev.data["playersArray"]);
         Debug.Log(ev.data["playersArray"][0]["name"]);
+        //update player positions
+    }
+
+    public int interval = 30;
+
+    void FixedUpdate() 
+    { 
+        if (Time.frameCount % interval == 0) {
+            PositionChange();
+         } 
+    }
+
+    void PositionChange()
+    {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["x"] = player.transform.position.x.ToString();
+        data["y"] = player.transform.position.x.ToString();
+        socket.Emit("positionChange", new JSONObject(data));
     }
 
 }
